190de00e477369aeba7205b1818f9faf1c6cadcb
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2024-03-05 07:54:41 +0100

    export graph loader to be reusable

--- src/exports.ts
+++ src/exports.ts
@@ -1,3 +1,6 @@
-export { Theme, InputListEntry, Editor } from "./theme"
+export { Theme, InputListEntry, Editor } from './theme'
+export { Loader } from './loader'
+export { Config } from './config'
 export { Plugin } from './plugin'
+export { ShaclPropertyTemplate } from './property-template'
 
