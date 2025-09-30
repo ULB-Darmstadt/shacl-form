810ddb8bcefb652cffc6108531c812cd0ac0ad79
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2025-09-30 11:35:11 +0200

    enabled unit tests

--- test/shacl-in.test.ts
+++ test/shacl-in.test.ts
@@ -2,40 +2,57 @@ import { html, fixture, expect } from '@open-wc/testing'
 import { ShaclForm } from '../src/form'
 import '../src/form'
 
+const listValues = ['<http://example.org>', '1000', '"2000"', '"string"@en', '"1900-01-01"^^xsd:date', '"http://example.org"^^xsd:anyUri']
 const shapes = `
+  @prefix :        <http://example.org/> .
   @prefix sh:      <http://www.w3.org/ns/shacl#> .
   @prefix xsd:     <http://www.w3.org/2001/XMLSchema#> .
-  @prefix ex:      <http://example.org/> .
 
-  ex:TestShape
+  :TestShape
     a sh:NodeShape ;
     sh:property [
-      sh:path ex:test ;
-      sh:in ( <http://example.org> 1000 "2000" "string"@en "1900-01-01"^^xsd:date "http://example.org"^^xsd:anyUri )
+      sh:path :path ;
+      sh:in ( ${listValues.join(' ')} )
     ] .
 `
+const data = (value: string) => `
+  @prefix :        <http://example.org/> .
+  @prefix xsd:     <http://www.w3.org/2001/XMLSchema#> .
 
-describe('MyElement', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el: ShaclForm = await fixture(html`
+  :data :path ${value} .
+`
+
+const awaitFormLoaded = (form: ShaclForm) => {
+  return new Promise<void>(resolve => {
+    const interval = setInterval(() => {
+      if (form.getAttribute('loading') === null) {
+        clearInterval(interval)
+        resolve()
+      }
+    }, 150)
+  })
+}
+
+describe('test sh:in value serialization', () => {
+  let form: ShaclForm
+
+  before(async () => {
+    form = await fixture(html`
       <shacl-form
         data-shapes="${shapes}"
         data-shape-subject="http://example.org/TestShape"
       ></shacl-form>`)
+  })
 
-    expect(el.title).to.equal('Hey there');
-    // expect(el.counter).to.equal(5);
-  });
-
-  it('can override the title via attribute', async () => {
-    const el: ShaclForm = await fixture(html` <shacl-form></shacl-form> `);
-
-    expect(el.title).to.equal('attribute title');
-  });
-
-  it('passes the a11y audit', async () => {
-    const el: ShaclForm = await fixture(html` <shacl-form></shacl-form> `);
-
-    await expect(el).shadowDom.to.be.accessible();
-  });
-});
+  it('list value serialize correctly', async () => {
+    for (const value of listValues) {
+      form.setAttribute('data-values-subject', 'http://example.org/data')
+      form.setAttribute('data-values', data(value))
+      await awaitFormLoaded(form)
+      const report = await form.validate()
+      expect(report.conforms, 'expect form to validate').to.be.true
+      const rdf = form.serialize()
+      expect(rdf.indexOf(value), `expect existance of value ${value} in:\n${rdf}`).to.be.greaterThan(-1)
+    }
+  })
+})
--- test/web-test-runner.config.mjs
+++ test/web-test-runner.config.mjs
@@ -1,7 +1,7 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { vitePlugin } from '@remcovaes/web-test-runner-vite-plugin';
 
 export default{
   files: 'test/**/*.test.ts',
   nodeResolve: true,
-  plugins: [esbuildPlugin({ ts: true })],
+  plugins: [vitePlugin()],
 };
