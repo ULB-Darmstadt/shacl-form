18b02071688dd456f76a86503586dda64fd9e2e7
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2025-10-01 11:05:29 +0200

    improve unit tests

--- test/web-test-runner.config.mjs
+++ test/web-test-runner.config.mjs
@@ -1,7 +1,14 @@
-import { vitePlugin } from '@remcovaes/web-test-runner-vite-plugin';
+import { vitePlugin } from '@remcovaes/web-test-runner-vite-plugin'
+import { chromeLauncher } from '@web/test-runner-chrome'
 
 export default{
   files: 'test/**/*.test.ts',
   nodeResolve: true,
   plugins: [vitePlugin()],
-};
+  filterBrowserLogs: ({args}) => !(args.length && (args[0].startsWith('Lit is in dev mode') || args[0].indexOf('rokit-select') > -1)),
+  browsers: [
+    chromeLauncher({
+      concurrency: 1, // Otherwise a lot of tests time out in Chrome
+    }),
+  ]
+}
