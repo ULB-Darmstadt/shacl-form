92e39cba79c1aba5099401c84b37f5d178ea950d
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2025-10-15 13:35:54 +0200

    add unit test for recursion prevention while parsing

---
+++ test/recursion.test.ts
@@ -0,0 +1,30 @@
+import { html, fixture, expect } from '@open-wc/testing'
+import { ShaclForm } from '../src/form'
+import { bind, expectIsomorphic, expectValid } from './util'
+import '../src/form'
+import { DataFactory, Quad } from 'n3'
+
+const prefixes = '@prefix : <http://example.org/> . @prefix sh: <http://www.w3.org/ns/shacl#> . @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .'
+
+describe('test recustion protection', () => {
+    let form: ShaclForm
+
+    before(async () => { form = await fixture(html`<shacl-form></shacl-form>`) })
+
+    it('parsing self-referencing shapes', async () => {
+        await bind(form, `
+            ${prefixes}
+            :Human a sh:NodeShape ;
+            sh:property [
+                sh:path :loves ;
+                sh:node :Dog ;
+            ] .
+            :Dog a sh:NodeShape ;
+            sh:property [
+                sh:path :loves ;
+                sh:node :Human ;
+            ] .            `
+        )
+        expect(form.shape?.template.id.value).to.equal('http://example.org/Human')
+    })
+})
