b0ffa705638789ce2077c84f95964d17f00cc629
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2026-02-04 10:59:27 +0100

    fix timeouts in unit tests

--- test/recursion.test.ts
+++ test/recursion.test.ts
@@ -6,7 +6,7 @@ import { DataFactory, Quad } from 'n3'
 
 const prefixes = '@prefix : <http://example.org/> . @prefix sh: <http://www.w3.org/ns/shacl#> . @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .'
 
-describe('test recustion protection', () => {
+describe('test recursion protection', () => {
     let form: ShaclForm
 
     before(async () => { form = await fixture(html`<shacl-form data-generate-node-shape-reference=""></shacl-form>`) })
