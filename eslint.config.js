c76339dd683ad982a2a59783b7a90299262578f7
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2026-02-10 10:15:40 +0100

    update eslint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+];
